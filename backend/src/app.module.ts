65a3e3a43c83129da0d74871de3eba3932181487
John Alex Ladra <johnalexladra@gmail.com>
2023-12-22 16:31:39 +0800

    Add auth in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MovieModule } from './movie/movie.module';
     PrismaModule,
     UserModule,
     MovieModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
