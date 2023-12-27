40fefae13a0ce618a28062883ef9cbc65c9c214c
John Alex Ladra <johnalexladra@gmail.com>
2023-12-27 08:56:27 +0800

    Create movie dto

--- backend/src/movie/dto/create-movie.dto.ts
+++ backend/src/movie/dto/create-movie.dto.ts
@@ -1,1 +1,23 @@
-export class CreateMovieDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  overview?: string;
+
+  @IsString()
+  @IsOptional()
+  homepage: string;
+
+  @IsString()
+  @IsOptional()
+  imgUrl: string;
+
+  @IsString()
+  @IsNotEmpty()
+  genre: string;
+}
--- backend/src/movie/dto/update-movie.dto.ts
+++ backend/src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,25 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+  @IsString()
+  @IsNotEmpty()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  overview?: string;
+
+  @IsString()
+  @IsOptional()
+  homepage?: string;
+
+  @IsString()
+  @IsOptional()
+  imgUrl: string;
+
+  @IsString()
+  @IsNotEmpty()
+  genre: string;
+}
