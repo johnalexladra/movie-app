d9964d88788819552a0dd5b92ef6b3fc3b3cb966
John Alex Ladra <johnalexladra@gmail.com>
2024-01-08 17:22:58 +0800

    update dashboard to client component

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { getRandomMedia, getTrending } from "@/services/movie-api";
 import MediaCarousel from "@/components/carousel/static";
 import MediaHero from "@/components/media/hero";
@@ -5,28 +7,46 @@ import { getFavoriteByType } from "@/services/server-api";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/services/next-auth";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from 'react';
+import { useSession } from "next-auth/react";
 
 export const revalidate = 60 * 60 * 24; // 24 hours
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Dashboard() {
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [favoriteSeries, setFavoriteSeries] = useState([]);
+  // const session = await getServerSession(authOptions);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session) {
+        const movies = await getFavoriteByType("movie", session?.backendTokens.accessToken as string);
+        const series = await getFavoriteByType("tv", session?.backendTokens.accessToken as string);
+        setFavoriteMovies(movies || []);
+        setFavoriteSeries(series || []);
+      }
+    };
+
+    fetchData();
+  }, [session]); 
 
   // Check if the session is empty or the session token has expired
   if(!session) {
     redirect('/');
   }
 
-  const trendingMovie = await getTrending("movie");
-  const trendingTv = await getTrending("tv");
-  const randomItem = await getRandomMedia([
-    ...trendingMovie.results,
-    ...trendingTv.results,
-  ]);
-  const favoriteMovies = await getFavoriteByType("movie", session?.backendTokens.accessToken as string);
-  const favoriteSeries = await getFavoriteByType("tv", session?.backendTokens.accessToken as string);
+  // const trendingMovie = await getTrending("movie");
+  // const trendingTv = await getTrending("tv");
+  // const randomItem = await getRandomMedia([
+  //   ...trendingMovie.results,
+  //   ...trendingTv.results,
+  // ]);
+  // const favoriteMovies = await getFavoriteByType("movie", session?.backendTokens.accessToken as string);
+  // const favoriteSeries = await getFavoriteByType("tv", session?.backendTokens.accessToken as string);
 
   return (
     <main>
-      <MediaHero media={randomItem} />
+      {/* <MediaHero media={randomItem} /> */}
       <div className="my-global space-y-5">
       {favoriteMovies && favoriteMovies.length > 0 && (
         <MediaCarousel
