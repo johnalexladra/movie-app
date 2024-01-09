222b13b38c78ca7775c05d20f87098d2c36cbae4
John Alex Ladra <johnalexladra@gmail.com>
2024-01-09 10:05:27 +0800

    add role in auth payload

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -87,7 +87,8 @@ export class AuthService {
     const payload = {
       id: user.id,
       email: user.email,
-      name: user.name
+      name: user.name,
+      role: user.role
     };
 
     return {
